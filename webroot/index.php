16c45d1b56a804d1a997aec26cbaaac4af938ef2
linearlyIndependent <35012155+linearlyIndependent@users.noreply.github.com>
2021-10-14 16:50:18 +0200

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,40 +1,20 @@
 <?php
-/**
- * The Front Controller for handling every request
- *
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.2.9
- * @license       MIT License (https://opensource.org/licenses/mit-license.php)
- */
+$servername = "mysql.hosting-test.cluster.local";
+$username = "karl_der_benutzer";
+$password = "karl_der_user_passwort";
+$dbname = "karl_der_datenbank";
+$dbport = "3306";
 
-// Check platform requirements
-require dirname(__DIR__) . '/config/requirements.php';
 
-// For built-in server
-if (php_sapi_name() === 'cli-server') {
-    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname, $dbport);
 
-    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
-    $file = __DIR__ . $url['path'];
-    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
-        return false;
-    }
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname;port=$dbport", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-use App\Application;
-use Cake\Http\Server;
-
-// Bind your application to the server.
-$server = new Server(new Application(dirname(__DIR__) . '/config'));
-
-// Run the request/response through the application and emit the response.
-$server->emit($server->run());
+?> 
